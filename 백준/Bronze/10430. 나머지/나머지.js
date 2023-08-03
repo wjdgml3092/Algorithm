870b99debd17aa5d9ea8b307b2526f61044c9988
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-08-03 22:47:25 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : './backjoon/test.txt'
+const [A, B, C] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map((item) => Number(item))
+
+console.log((A + B) % C)
+console.log(((A % C) + (B % C)) % C)
+console.log((A * B) % C)
+console.log(((A % C) * (B % C)) % C)
