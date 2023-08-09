2277092294e78c21e5aa5c44ee2aa284745a6f56
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-08-10 00:57:49 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 15.95 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    let answer = []
+    let stack = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if(!stack.includes(s[i])){
+            answer.push(-1);
+            stack.push(s[i]);
+            continue;
+        }
+        
+        if(stack.includes(s[i])){
+            answer.push(stack.length - stack.lastIndexOf(s[i]));
+            stack.push(s[i]);
+            continue;
+        }
+    }
+    
+    return answer;
+}
