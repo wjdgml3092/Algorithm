69d0e84aa2ee0fa6949d4106a4218fd54e20833e
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-10-12 17:37:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 308 ms, Memory: 10528 KB -BaekjoonHub

---
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.js
@@ -0,0 +1,34 @@
+const { exit } = require('process')
+const fs = require('fs')
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : './backjoon/24051_test.txt'
+const input = fs.readFileSync(filePath).toString().split('\n')
+
+let cnt = Number(input[0].split(' ')[1])
+let arr = input[1].split(' ').map((item) => Number(item))
+function insertSort(arr) {
+  let changeCnt = 0
+
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i]
+    let j
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      if (changeCnt === cnt) {
+        return arr[j + 1]
+      }
+
+      arr[j + 1] = arr[j]
+
+      changeCnt += 1
+    }
+
+    if (j + 1 !== i) {
+      arr[j + 1] = currentVal
+      changeCnt += 1
+    }
+  }
+
+  return -1
+}
+
+console.log(insertSort(arr))
