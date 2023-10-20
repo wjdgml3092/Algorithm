c7315e18f2a975904dd885c4c0681837c8853931
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-10-20 18:40:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,12 +1,13 @@
 function solution(s) {
-    let arr = s.split(" ").map((item) => Number(item));
     
-    let min = arr[0], max = arr[0];
+    let min = Infinity, max = -Infinity;
     
-    arr.forEach((item) => {
-        max = Math.max(max,item)
-        min = Math.min(min,item) 
-    })
+    s.split(" ").forEach((item) => {
+        const number = Number(item)
+        
+        max = Math.max(max, number)
+        min = Math.min(min, number)
+    });
     
     return `${min} ${max}`;
 }
